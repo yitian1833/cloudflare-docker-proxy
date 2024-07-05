81200d48508f45d425698e0f810643de02fb19c7
yitian1833 <118341269+yitian1833@users.noreply.github.com>
2024-07-05 11:09:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tangtalk.asia": dockerHub,
+  "quay.tangtalk.asia": "https://quay.io",
+  "gcr.tangtalk.asia": "https://gcr.io",
+  "k8s-gcr.tangtalk.asia": "https://k8s.gcr.io",
+  "k8s.tangtalk.asia": "https://registry.k8s.io",
+  "ghcr.tangtalk.asia": "https://ghcr.io",
+  "cloudsmith.tangtalk.asia": "https://docker.cloudsmith.io",
+  "ecr.tangtalk.asia": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tangtalk.asia": dockerHub,
 };
 
 function routeByHosts(host) {
